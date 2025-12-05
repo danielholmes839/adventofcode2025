ed7f45aed396b4d6cc22c253b385e058770983a4
Daniel Holmes <danielh839@gmail.com>
2025-12-05 18:19:36 -0500

    day 4 part 2

--- src/day4.py
+++ src/day4.py
@@ -1,38 +1,62 @@
 offsets = [(0, 1), (-1, -1), (-1, 1), (1, 1), (1, -1), (-1, 0), (1, 0), (0, -1)]
 
+
+
+def remove(lines) -> int:
+    removed = 0
+
+    for r, line in enumerate(lines):
+        for c, val in enumerate(line):
+            if val != "@":
+                # print(".", end="")
+                continue
+
+            adjacent_count = 0
+
+            for dr, dc in offsets:
+                nr, nc = r + dr, c + dc
+                if nr < 0 or nc < 0 or nr >= len(lines) or nc >= len(line):
+                    continue
+
+                adjacent = lines[nr][nc]
+
+                if adjacent == "@":
+                    # print("@")
+                    adjacent_count += 1
+
+            if adjacent_count < 4:
+                lines[r][c] = "."
+                removed += 1
+    
+    return removed
+
+
 with open('data/day4.txt', 'r') as f:
     lines = f.read().splitlines()
 
 
-part1 = 0
+lines = [list(line) for line in lines]
+print(lines[0])
 
-for r, line in enumerate(lines):
-    for c, val in enumerate(line):
-        if val != "@":
-            # print(".", end="")
-            continue
+removed = 0
 
-        adjacent_count = 0
+while True:
+    new_removed = remove(lines)
+    if new_removed == 0:
+        break
 
-        for dr, dc in offsets:
-            nr, nc = r + dr, c + dc
-            if nr < 0 or nc < 0 or nr >= len(lines) or nc >= len(line):
-                continue
+    removed += new_removed
 
-            adjacent = lines[nr][nc]
+print(removed)
 
-            if adjacent == "@":
-                # print("@")
-                adjacent_count += 1
 
-        if adjacent_count < 4:
-            part1 += 1
-            
+
             # print(adjacent_count, end="")
             # print("x", end="")
         # else:
             
             # print(adjacent_count, end="")
             # print("@", end="")
+    
 
-print(part1)
+print(removed)
